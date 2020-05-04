82cb8b65d5b25bd9cc67566a5ce1d8edbdff685e
mmille014 <miguel_miller@cable.comcast.com>
2020-05-04 17:11:06 -0400

    Chap 7 Inheritance QuizRunner

--- src/org/launchcode/java/studios/inheritance/Questions.java
+++ src/org/launchcode/java/studios/inheritance/Questions.java
@@ -14,21 +14,3 @@ public class Questions {
     }
 }
 
-
-//Public class Computer {
-//    private String name;
-//    private Stringspeed;
-//    private Integer age;
-//
-//    public  Computer(String name, String speed, Integer age) {
-//        this.name = name;
-//        this.speed = speed;
-//        this.age = age;
-//    }
-//    public static void startUpTime() {
-//        System.out.println("The computer's startup time is 10 seconds.");
-//    }
-//    public static void seating() {
-//        System.out.println("A leather office chair is utilized for the computer.");
-//    }
-//}

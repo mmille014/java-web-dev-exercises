dd4b241c0b7dbf4cb695d3d595e93e00b00e1803
mmille014 <miguel_miller@cable.comcast.com>
2020-05-05 13:29:40 -0400

    chap.8 Exercise

---
+++ src/org/launchcode/java/demos/lsn7interfaces/ConeComparator.java
@@ -0,0 +1,13 @@
+//package org.launchcode.java.demos.lsn7interfaces;
+//
+//import java.util.Comparator;
+//
+//public class ConeComparator implements Comparator<Cost>{
+//
+//    @Override
+//    public int compare(Cost o1, Cost o2) {
+//        return o1.geCost().compareTo(o2.getCost());
+//        cones.sort(comparator);
+//        if (o1<o2
+//    }
+//}

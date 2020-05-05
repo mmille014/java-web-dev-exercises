b01014b5c41b1745b7fe89c9a3d3ef259f7237d4
mmille014 <miguel_miller@cable.comcast.com>
2020-05-05 09:33:53 -0400

    update 1

--- src/org/launchcode/java/studios/inheritance/MultipleChoice.java
+++ src/org/launchcode/java/studios/inheritance/MultipleChoice.java
@@ -1,4 +1,7 @@
 package org.launchcode.java.studios.inheritance;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class MultipleChoice {
 }
--- src/org/launchcode/java/studios/inheritance/QuizRunner.java
+++ src/org/launchcode/java/studios/inheritance/QuizRunner.java
@@ -1,9 +1,28 @@
-package org.launchcode.java.studios.inheritance;
-
-import java.util.Scanner;
-
-public class QuizRunner {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-    }
-}
+//package org.launchcode.java.studios.inheritance;
+//
+//import java.util.Scanner;
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//
+//public class QuizRunner {
+//    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//
+//        //create quiz
+//        Quiz myQuiz = new Quiz(new ArrayList<>());
+//
+//        //quiz questions:
+//        ArrayList<String> possAnswer =
+//                new ArrayList<>(Arrays.asList("true-false", "false-false", "true-true", "false-true"));
+//                MultipleChoice newQuestion1 =
+//                        new MultipleChoice();
+//
+//                myQuiz.addQuestion(newQuestion1);
+//
+//
+//                //run quiz
+//                myQuiz.runQuiz();
+//
+//                myQuiz.gradeQuiz();
+//    }
+//}

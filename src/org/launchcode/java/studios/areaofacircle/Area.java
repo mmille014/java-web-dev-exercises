4a7b6a365cfa13c2b13684176d68c6a7289a1f72
mmille014 <miguel_miller@cable.comcast.com>
2020-11-29 15:31:43 -0500

    Completed Area of a circle minus the bonus mission.

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -3,13 +3,24 @@ package org.launchcode.java.studios.areaofacircle;
 import java.util.Scanner;
 
 public class Area {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Please enter the radius of the circle: ");
+        System.out.println("Enter a radius: ");
         double radius = input.nextDouble();
-        double area = Math.PI * Math.pow(radius, 2);
 
-        System.out.println("The area of a circle of radius " + radius + " is: " + area + ".");
+        //Removed the following 2 lines because I added a Circle class and made a call to that class for the same information.
+        //double pi = 3.14;
+        //double area = pi * radius * radius;
+
+        Circle.getArea(radius);
+
+        System.out.println("The area of a circle of radius " + radius + " is " + Circle.getArea(radius));
+
+        // Did Not Include The Bonus Mission:
+        // 2.5.4. Bonus Missions¶
+        // Add validation to your program. If the user enters a negative number? a non-numeric character? the empty string? Print an error message and quit. You’ll need to peek ahead to learn about conditional syntax in Java.
+        // Extend your program further by using a while or do-while loop, so that when the user enters a negative number they are re-prompted.
     }
 }
--- src/org/launchcode/java/studios/areaofacircle/Circle.java
+++ src/org/launchcode/java/studios/areaofacircle/Circle.java
@@ -1,9 +1,7 @@
 package org.launchcode.java.studios.areaofacircle;
 
-import java.util.Scanner;
-
 public class Circle {
-    public static double getArea(double radius) {
-        return Math.PI * Math.pow(radius, 2);
+    public static Double getArea(Double radius) {
+        return 3.14 * radius * radius;
     }
 }
